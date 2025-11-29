be9ae65176c5ee857165808fee4acbcf7a68dc29
dlurena24 <dlurena24@gmail.com>
2025-11-29 14:53:30 -0600

    Sistema de aparicion por probabilidad agregado
    Agregado un sistema de aparicion por probabilidad para los avatars

--- avatars-vs-rooks/Framework/nivel_1.gd
+++ avatars-vs-rooks/Framework/nivel_1.gd
@@ -5,13 +5,16 @@ extends Node2D
 @onready var cursor_rook: Node2D = $Cursor_Rook
 @onready var rooks: Node2D = $Rooks
 
-@onready var timer_avatar_escudero: Timer = $TimerAvatarEscudero
+@onready var timer_avatar_spawn: Timer = $TimerAvatarEscudero
 @onready var timer_monedas: Timer = $TimerMonedas
 @onready var timer_nivel: Timer = $TimerNivel
 
 
 # Avatars
 @export var escudero : PackedScene
+@export var flechador : PackedScene
+@export var lenador : PackedScene
+@export var canival : PackedScene
 
 @export var moneda : PackedScene
 
@@ -45,8 +48,8 @@ func _ready():
 	timer_nivel.start()
 	timer_nivel.timeout.connect(ganar_nivel)
 	
-	timer_avatar_escudero.start()
-	timer_avatar_escudero.timeout.connect(spawnear_escudero)
+	timer_avatar_spawn.start()
+	timer_avatar_spawn.timeout.connect(spawnear_avatar)
 	
 	timer_monedas.start()
 	timer_monedas.timeout.connect(spawnear_moneda)
@@ -64,7 +67,23 @@ func crear_celdas():
 			nueva_celda.position = Vector2(48.5, 48) + (Vector2(x,y) * Vector2(97, 96))
 			nueva_celda.posicion_celda = Vector2i(x,y)
 			
+
+var avatars = ["flechador", "escudero", "lenador", "canival"]
+var weights = [40, 30, 20, 10]  
+
+func weighted_choice(options: Array, weights: Array):
+	var total := 0
+	for w in weights:
+		total += w
 	
+	var r := randi() % total
+	var cumulative := 0
+	
+	for i in range(options.size()):
+		cumulative += weights[i]
+		if r < cumulative:
+			return options[i]
+			
 func spawnear_escudero():
 	var escudero_a_colocar = escudero.instantiate()
 	get_tree().current_scene.add_child(escudero_a_colocar)
@@ -72,6 +91,40 @@ func spawnear_escudero():
 	var marcador = marcadoresAvatars[marker_pos]
 	escudero_a_colocar.global_position = marcador.global_position
 	
+func spawnear_flechador():
+	var flechador_a_colocar = flechador.instantiate()
+	get_tree().current_scene.add_child(flechador_a_colocar)
+	var marker_pos = randi_range(0, 4)
+	var marcador = marcadoresAvatars[marker_pos]
+	flechador_a_colocar.global_position = marcador.global_position
+	
+func spawnear_canival():
+	var canival_a_colocar = canival.instantiate()
+	get_tree().current_scene.add_child(canival_a_colocar)
+	var marker_pos = randi_range(0, 4)
+	var marcador = marcadoresAvatars[marker_pos]
+	canival_a_colocar.global_position = marcador.global_position
+	
+func spawnear_lenador():
+	var lenador_a_colocar = lenador.instantiate()
+	get_tree().current_scene.add_child(lenador_a_colocar)
+	var marker_pos = randi_range(0, 4)
+	var marcador = marcadoresAvatars[marker_pos]
+	lenador_a_colocar.global_position = marcador.global_position
+	
+func spawnear_avatar():
+	var chosen_room = weighted_choice(avatars, weights)
+
+	match chosen_room:
+		"flechador":
+			spawnear_flechador()
+		"escudero":
+			spawnear_escudero()
+		"lenador":
+			spawnear_lenador()
+		"canival":
+			spawnear_canival()
+
 func spawnear_moneda():
 	var moneda_a_colocar = moneda.instantiate()
 	get_tree().current_scene.add_child(moneda_a_colocar)
